85e8e277ccfb8ad45f39476a44c8dd7f40ea9bdc
Thiago Barbosa <sbarbosathiago@gmail.com>
2025-11-04 18:11:08 +0000

    inclusão do arquivo `.prettierignore` e edição de `index.js`

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Oi, Yara! Te amo 🖤🖤🖤</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
 export default Home;
