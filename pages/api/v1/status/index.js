edb2ac7685fcf85ae846c9b925420ddb9c5ec023
Thiago Barbosa <sbarbosathiago@gmail.com>
2025-11-24 16:22:01 +0000

    add `database.js`and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-    response.status(200).json({ "chave":"status de thbarbosa.com.br" }) // json para habilitar UTF-8
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+    const result = await database.query("SELECT 1 + 1 AS query_result;");
+    console.log(result.rows);
+    response.status(200).json({ "chave":"status de thbarbosa.com.br" }); // json para habilitar UTF-8
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,3 +2,5 @@ test("GET to /api/v1/status should return 200", async() => {
     const response = await fetch("http://localhost:3000/api/v1/status")
     expect(response.status).toBe(200)
 });
+
+// `npm run test:watch` é o comando para deixar os testes rodando em modo watch
